e50ac638b615797718bee9eed62054f5643b09e1
josemarpedro758 <josemarpedrog@gmail.com>
2026-06-09 16:47:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -736,6 +736,27 @@ app.post(
 
       await pool.query(
 
+  `
+  INSERT INTO messages
+  (
+    number,
+    message,
+    response
+  )
+
+  VALUES($1,$2,$3)
+  `,
+
+  [
+    number,
+    message,
+    aiMessage
+  ]
+
+);
+
+      await pool.query(
+
         `
         INSERT INTO clients
         (
